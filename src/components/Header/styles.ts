7d438db49d521ef68bf920491931a30ac771d7ad
caiohenr1 <caiohenrique-dev@outlook.com>
2023-05-17 16:48:48 -0300

    components criados, css global, rotas , ts

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  nav {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+
+    a {
+      width: 3rem;
+      height: 3rem;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      text-decoration: none;
+      color: ${props => props.theme["gray-100"]};
+
+      border-top: 3px solid transparent;
+      border-bottom: 3px solid transparent;
+    }
+
+  }
+
+  a {
+    &:hover {
+      border-bottom: 3px solid ${props => props.theme["green-500"]};
+    }
+
+    &:focus {
+      outline: none;
+      box-shadow: none;
+    }
+
+    &.active {
+      color: ${props => props.theme["green-500"]};
+    }
+  }
+`
